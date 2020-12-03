0bc714009e01c5a75bab9a700eb2c17440789023
Felipe Randolfi <fdrandolfi@gmail.com>
2020-12-02 23:13:32 -0300

    [Feature] Update Repository (#4)
    * add eslint config and set readme
    
    * add new deps and update old deps
    
    * add new preview logotype
    
    * add new sass basic files
    
    * update linter code review
    
    * add sasslint config

--- src/client/components/ExampleComponent/index.js
+++ src/client/components/ExampleComponent/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import "./index.scss";
-import logotype from "../../assets/images/logotype.png";
+import './index.scss';
+import logotype from '../../assets/images/logotype.png';
 
 const ExampleComponent = () => (
   <div className="example">
-    <img src={logotype} />
+    <img src={logotype} alt="logotype" />
   </div>
 );
 
